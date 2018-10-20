42ecfa950e0d7c57343a2079f9c740ccb4319533
Gian Ortiz <brksgian@gmail.com>
2018-10-20 11:51:49 -0300

    Added tree data model functions

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,87 @@ import * as cp from 'child_process'
 import * as http from 'http'
 import * as vscode from 'vscode'
 
+class Node {
+    value: string
+    nodes: object
+    search: boolean
+    constructor(setValue: string, wasSearched: boolean = false) {
+        this.value = setValue
+        this.nodes = {}
+        this.search = wasSearched
+    }
+}
+
+function createNewQueryBranch(array: object, data: object, query: string, index) {
+    if(data['nodes'][query]) {
+        if(array[index+1]) {
+            let nextNode = getNode(data, query)
+            index += 1
+            query += array[index]
+            return createNewQueryBranch(array, nextNode, query, index)
+        } else {
+            data['search'] = true
+            return
+        }
+    } else {
+        if(array[index+1]) {
+            addNode(data, query, false)
+            let nextNode = getNode(data, query)
+            index += 1
+            query += array[index]
+            return createNewQueryBranch(array, nextNode, query, index)
+        } else {
+            addNode(data, query, true)
+            return 
+        }
+    }
+}
+
+function getNode(tree: object, name: string) {
+    return tree['nodes'][name] ? tree['nodes'][name] : null
+}
+
+function addNode(tree: object, nodeName: string, wasSearched: boolean) {
+    if(!tree['nodes'][nodeName]) {
+        let node = new Node(nodeName, wasSearched)
+        tree['nodes'][nodeName] = node
+    }
+}
+
+function getQuerySearchedNodes(tree: object, query: string) {
+    let queryArray = query.split('')
+    let tempQuery = ''
+    let tempNode = tree
+    let getToFinalLeave = false
+    queryArray.forEach( element => {
+        tempQuery += element
+        if(tempNode['nodes'][tempQuery]) {
+            tempNode = tempNode['nodes'][tempQuery]
+            getToFinalLeave = false
+        } else {
+            getToFinalLeave = true
+        }
+    })
+    if(getToFinalLeave) {
+        return []
+    } else {
+        let searchedQueries = []
+        getAllSearchedNodes(tempNode, searchedQueries)
+        return searchedQueries
+    }
+}
+
+function getAllSearchedNodes(tree: object, searchedQueries = []) {
+    for(var key in tree['nodes']) {
+        if(tree['nodes'][key]['search']) {
+            searchedQueries.push(tree['nodes'][key]['value'])
+            getAllSearchedNodes(tree['nodes'][key], searchedQueries)
+        } else {
+            getAllSearchedNodes(tree['nodes'][key], searchedQueries)
+        }
+    }
+}
+
 const cache = {
     state: <vscode.Memento> null
 }
@@ -105,8 +186,12 @@ function asyncRequest(queryRaw: string, callback: (data: string) => void) {
             data += chunk
         })
 
-        message.on("end", function () {
+        message.on("end",  async function () {
             requestCache[path] = data
+            let cacheData = await cache.state.get(`snippet_${language}`)
+            let queryArray = query.split('')
+            createNewQueryBranch(queryArray, cacheData, queryArray[0], 0)
+            cache.state.update(`snippet_${language}`, cacheData)
             loadingStatus.hide()
             callback(data)
         })
