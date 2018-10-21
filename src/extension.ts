bcaeb253d666713c3045750678f61d51eb047ad0
Gian Ortiz <brksgian@gmail.com>
2018-10-21 11:23:37 -0300

    Substitute inputBox by quickPick

--- src/extension.ts
+++ src/extension.ts
@@ -3,87 +3,6 @@ import * as cp from 'child_process'
 import * as http from 'http'
 import * as vscode from 'vscode'
 
-class Node {
-    value: string
-    nodes: object
-    search: boolean
-    constructor(setValue: string, wasSearched: boolean = false) {
-        this.value = setValue
-        this.nodes = {}
-        this.search = wasSearched
-    }
-}
-
-function createNewQueryBranch(array: object, data: object, query: string, index) {
-    if(data['nodes'][query]) {
-        if(array[index+1]) {
-            let nextNode = getNode(data, query)
-            index += 1
-            query += array[index]
-            return createNewQueryBranch(array, nextNode, query, index)
-        } else {
-            data['search'] = true
-            return
-        }
-    } else {
-        if(array[index+1]) {
-            addNode(data, query, false)
-            let nextNode = getNode(data, query)
-            index += 1
-            query += array[index]
-            return createNewQueryBranch(array, nextNode, query, index)
-        } else {
-            addNode(data, query, true)
-            return 
-        }
-    }
-}
-
-function getNode(tree: object, name: string) {
-    return tree['nodes'][name] ? tree['nodes'][name] : null
-}
-
-function addNode(tree: object, nodeName: string, wasSearched: boolean) {
-    if(!tree['nodes'][nodeName]) {
-        let node = new Node(nodeName, wasSearched)
-        tree['nodes'][nodeName] = node
-    }
-}
-
-function getQuerySearchedNodes(tree: object, query: string) {
-    let queryArray = query.split('')
-    let tempQuery = ''
-    let tempNode = tree
-    let getToFinalLeave = false
-    queryArray.forEach( element => {
-        tempQuery += element
-        if(tempNode['nodes'][tempQuery]) {
-            tempNode = tempNode['nodes'][tempQuery]
-            getToFinalLeave = false
-        } else {
-            getToFinalLeave = true
-        }
-    })
-    if(getToFinalLeave) {
-        return []
-    } else {
-        let searchedQueries = []
-        getAllSearchedNodes(tempNode, searchedQueries)
-        return searchedQueries
-    }
-}
-
-function getAllSearchedNodes(tree: object, searchedQueries = []) {
-    for(var key in tree['nodes']) {
-        if(tree['nodes'][key]['search']) {
-            searchedQueries.push(tree['nodes'][key]['value'])
-            getAllSearchedNodes(tree['nodes'][key], searchedQueries)
-        } else {
-            getAllSearchedNodes(tree['nodes'][key], searchedQueries)
-        }
-    }
-}
-
 const cache = {
     state: <vscode.Memento> null
 }
@@ -107,12 +26,38 @@ function find() {
 }
 
 function query(openInNewEditor: boolean) {
-    vscode.window.showInputBox()
-        .then(query => {
-            asyncRequest(query, function (data) {
+    let language = vscode.window.activeTextEditor.document.languageId
+    let tree = cache.state.get(`snippet_${language}`)
+    let suggestions = []
+    suggestions.sort()
+    getAllSearchedNodes(tree, suggestions)
+
+    const suggestionsQuickItems: Array<vscode.QuickPickItem> = []
+    let tempQuickItem: vscode.QuickPickItem = null
+    for(var key in suggestions) {
+        tempQuickItem = {label: suggestions[key]}
+        suggestionsQuickItems.push(tempQuickItem)
+    }
+
+    const quickPick = vscode.window.createQuickPick()
+    quickPick.items = suggestionsQuickItems
+    quickPick.onDidChangeValue(() => {
+        quickPick.activeItems = []
+    })
+    quickPick.onDidAccept(() => {
+        if(quickPick.activeItems.length) {
+            asyncRequest(quickPick.activeItems[0]['label'], function (data) {
                 insertText(data, openInNewEditor)
             })
-        });
+        } else {
+            asyncRequest(quickPick.value, function (data) {
+                insertText(data, openInNewEditor)
+            })
+        }
+        quickPick.hide()
+        quickPick.dispose()
+    })
+    quickPick.show()
 }
 
 function findInplace() {
@@ -188,10 +133,13 @@ function asyncRequest(queryRaw: string, callback: (data: string) => void) {
 
         message.on("end",  async function () {
             requestCache[path] = data
-            let cacheData = await cache.state.get(`snippet_${language}`)
-            let queryArray = query.split('')
-            createNewQueryBranch(queryArray, cacheData, queryArray[0], 0)
-            cache.state.update(`snippet_${language}`, cacheData)
+            let cacheData = cache.state.get(`snippet_${language}`)
+            // can't have undefined or '' in the QuickPick
+            if(query !== undefined && query !== '') {
+                let queryArray = queryRaw.split('')
+                createNewQueryBranch(queryArray, cacheData, queryArray[0], 0)
+                cache.state.update(`snippet_${language}`, cacheData)
+            }
             loadingStatus.hide()
             callback(data)
         })
@@ -221,5 +169,63 @@ function insertText(content: string, openInNewEditor = true) {
                 }
             )
         );
+    }
+}
+
+class Node {
+    value: string
+    nodes: object
+    search: boolean
+    constructor(setValue: string, wasSearched: boolean = false) {
+        this.value = setValue
+        this.nodes = {}
+        this.search = wasSearched
+    }
+}
+
+function createNewQueryBranch(array: object, data: object, query: string, index) {
+    if(data['nodes'][query]) {
+        if(array[index+1]) {
+            let nextNode = getNode(data, query)
+            index += 1
+            query += array[index]
+            return createNewQueryBranch(array, nextNode, query, index)
+        } else {
+            data['search'] = true
+            return
+        }
+    } else {
+        if(array[index+1]) {
+            addNode(data, query, false)
+            let nextNode = getNode(data, query)
+            index += 1
+            query += array[index]
+            return createNewQueryBranch(array, nextNode, query, index)
+        } else {
+            addNode(data, query, true)
+            return 
+        }
+    }
+}
+
+function getNode(tree: object, name: string) {
+    return tree['nodes'][name] ? tree['nodes'][name] : null
+}
+
+function addNode(tree: object, nodeName: string, wasSearched: boolean) {
+    if(!tree['nodes'][nodeName]) {
+        let node = new Node(nodeName, wasSearched)
+        tree['nodes'][nodeName] = node
+    }
+}
+
+function getAllSearchedNodes(tree: object, searchedQueries = []) {
+    for(var key in tree['nodes']) {
+        if(tree['nodes'][key]['search']) {
+            searchedQueries.push(tree['nodes'][key]['value'])
+            getAllSearchedNodes(tree['nodes'][key], searchedQueries)
+        } else {
+            getAllSearchedNodes(tree['nodes'][key], searchedQueries)
+        }
     }
 }
