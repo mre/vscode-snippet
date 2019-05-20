6702552d3f35ee73c21c0b7d5b1b514866fa2c7c
Matthias Endler <matthias-endler@gmx.net>
2019-05-21 00:16:59 +0200

    Add support for show next and previous answers

--- src/extension.ts
+++ src/extension.ts
@@ -5,11 +5,16 @@ import * as vscode from 'vscode'
 import * as HttpProxyAgent from 'http-proxy-agent'
 
 let loadingStatus = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left)
-loadingStatus.text =  'Loading Snippet ...'
+loadingStatus.text = 'Loading Snippet ...'
 
 const cache = {
-    state: <vscode.Memento> null
+    state: <vscode.Memento>null
 }
+
+// Last query string that was executed (not escaped)
+var currQuery = null
+// Last answer number that was executed
+var currNum = 0
 
 export function activate(ctx: vscode.ExtensionContext) {
     cache.state = ctx.globalState
@@ -21,6 +26,10 @@ export function activate(ctx: vscode.ExtensionContext) {
         'snippet.findInNewEditor', findInNewEditor))
     ctx.subscriptions.push(vscode.commands.registerCommand(
         'snippet.findSelectedText', findSelectedText))
+    ctx.subscriptions.push(vscode.commands.registerCommand(
+        'snippet.showPreviousAnswer', showPreviousAnswer))
+    ctx.subscriptions.push(vscode.commands.registerCommand(
+        'snippet.showNextAnswer', showNextAnswer))
 }
 
 function find() {
@@ -32,21 +41,21 @@ function find() {
 function query(openInNewEditor: boolean) {
     let language = vscode.window.activeTextEditor.document.languageId
     let tree = cache.state.get(`snippet_${language}`)
-    if(!tree) {
+    if (!tree) {
         let newTree = {}
         cache.state.update(`snippet_${language}`, newTree)
         tree = cache.state.get(`snippet_${language}`)
     }
     let suggestions = []
-    for(var key in tree) {
+    for (var key in tree) {
         suggestions.push(tree[key])
     }
     suggestions.sort()
-    
+
     let suggestionsQuickItems: Array<vscode.QuickPickItem> = []
     let tempQuickItem: vscode.QuickPickItem = null
-    for(var key in suggestions) {
-        tempQuickItem = {description: '', label: suggestions[key]}
+    for (var key in suggestions) {
+        tempQuickItem = { description: '', label: suggestions[key] }
         suggestionsQuickItems.push(tempQuickItem)
     }
 
@@ -57,12 +66,12 @@ function query(openInNewEditor: boolean) {
         quickPick.activeItems = []
     })
     quickPick.onDidAccept(() => {
-        if(quickPick.activeItems.length) {
-            asyncRequest(quickPick.activeItems[0]['label'], function (data) {
+        if (quickPick.activeItems.length) {
+            asyncRequest(quickPick.activeItems[0]['label'], 0, function (data) {
                 insertText(data, openInNewEditor)
             })
         } else {
-            asyncRequest(quickPick.value, function (data) {
+            asyncRequest(quickPick.value, 0, function (data) {
                 insertText(data, openInNewEditor)
             })
         }
@@ -80,6 +89,48 @@ function findInNewEditor() {
     query(true)
 }
 
+function showNextAnswer() {
+    let editor = vscode.window.activeTextEditor
+    if (!editor) {
+        vscode.window.showErrorMessage('There is no open editor window');
+        return
+    }
+
+    let selection = editor.selection;
+    let query = editor.document.getText(selection);
+
+    let configuration = vscode.workspace.getConfiguration('snippet')
+    let openInNewEditor: boolean = configuration["openInNewEditor"]
+
+    currNum += 1;
+    query = currQuery
+    asyncRequest(query, currNum, function (data) {
+        insertText(data, openInNewEditor)
+    })
+}
+
+function showPreviousAnswer() {
+    let editor = vscode.window.activeTextEditor
+    if (!editor) {
+        vscode.window.showErrorMessage('There is no open editor window');
+        return
+    }
+
+    let selection = editor.selection;
+    let query = editor.document.getText(selection);
+
+    let configuration = vscode.workspace.getConfiguration('snippet')
+    let openInNewEditor: boolean = configuration["openInNewEditor"]
+
+    if (currNum > 0) {
+        currNum -= 1;
+    }
+    query = currQuery
+    asyncRequest(query, currNum, function (data) {
+        insertText(data, openInNewEditor)
+    })
+}
+
 function findSelectedText() {
     let editor = vscode.window.activeTextEditor
     if (!editor) {
@@ -93,23 +144,27 @@ function findSelectedText() {
     let configuration = vscode.workspace.getConfiguration('snippet')
     let openInNewEditor: boolean = configuration["openInNewEditor"]
 
-    asyncRequest(query, function (data) {
+    asyncRequest(query, 0, function (data) {
         insertText(data, openInNewEditor)
     })
 }
 
 
 var requestCache = new Object()
-function asyncRequest(queryRaw: string, callback: (data: string) => void) {
+function asyncRequest(queryRaw: string, num: number, callback: (data: string) => void) {
+
+    currQuery = queryRaw
+    currNum = num
+
     loadingStatus.show()
 
     try {
         let query = encodeURI(queryRaw.replace(/ /g, '+'))
-    } catch(TypeError) {
+    } catch (TypeError) {
         loadingStatus.hide()
         return
     }
-    
+
     let query = encodeURI(queryRaw.replace(/ /g, '+'))
     let language = vscode.window.activeTextEditor.document.languageId
 
@@ -122,7 +177,7 @@ function asyncRequest(queryRaw: string, callback: (data: string) => void) {
 
     let baseUrl: String = configuration["baseUrl"]
 
-    let path = `/vscode:${language}/${query}?${params}&style=bw`;
+    let path = `/vscode:${language}/${query}/${num}?${params}&style=bw`;
 
     let data = requestCache[path]
     if (data) {
@@ -147,7 +202,7 @@ function asyncRequest(queryRaw: string, callback: (data: string) => void) {
     let httpConfiguration = vscode.workspace.getConfiguration('http')
     let proxy = httpConfiguration['proxy']
 
-    if(proxy !== ''){
+    if (proxy !== '') {
         let agent = new HttpProxyAgent(proxy)
         opts['agent'] = agent
     }
@@ -162,8 +217,8 @@ function asyncRequest(queryRaw: string, callback: (data: string) => void) {
         message.on("end", function () {
             requestCache[path] = data
             let cacheData = cache.state.get(`snippet_${language}`)
-            if(query !== undefined && query !== '') {
-                if(!cacheData[queryRaw]) {
+            if (query !== undefined && query !== '') {
+                if (!cacheData[queryRaw]) {
                     cacheData[queryRaw] = queryRaw
                     cache.state.update(`snippet_${language}`, cacheData)
                 }
