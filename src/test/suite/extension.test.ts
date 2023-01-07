9c3d4e785d45f674acf1dd83124e93a0c2f8b69d
VladBrok <vlad.brok99@gmail.com>
2023-01-07 10:40:10 +0300

    Add more tests for the snippet.findSelectedText command

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,37 +1,66 @@
 import * as assert from "assert";
 import * as vscode from "vscode";
-import { after } from "mocha";
+import { after, before } from "mocha";
 
 suite("Extension Test Suite", () => {
   suite("snippet.findSelectedText", () => {
-    after(async () => {
-      await vscode.commands.executeCommand("workbench.action.closeAllEditors");
-    });
-
-    test("Opens result in the new editor if openInNewEditor is true", async () => {
+    suite("openInNewEditor is true", () => {
       const queryText = "query";
-      const language = "js";
-      const doc = await vscode.workspace.openTextDocument({
-        language,
-        content: queryText,
+      const language = "javascript";
+
+      before(async () => {
+        const document = await vscode.workspace.openTextDocument({
+          language,
+          content: queryText,
+        });
+        await vscode.window.showTextDocument(document);
+        vscode.window.activeTextEditor.selection = new vscode.Selection(
+          0,
+          0,
+          0,
+          queryText.length
+        );
+        const config = vscode.workspace.getConfiguration("snippet");
+        await config.update("openInNewEditor", true, true);
+
+        await vscode.commands.executeCommand("snippet.findSelectedText");
       });
-      await vscode.window.showTextDocument(doc);
-      vscode.window.activeTextEditor.selection = new vscode.Selection(
-        0,
-        0,
-        0,
-        queryText.length
-      );
-      const config = vscode.workspace.getConfiguration("snippet");
-      await config.update("openInNewEditor", true, true);
 
-      await vscode.commands.executeCommand("snippet.findSelectedText");
+      after(async () => {
+        await vscode.commands.executeCommand(
+          "workbench.action.closeAllEditors"
+        );
+      });
 
-      assert.strictEqual(
-        vscode.window.activeTextEditor.document.getText(),
-        queryText
-      );
-      assert.strictEqual(vscode.workspace.textDocuments.length, 2);
+      test("Opens a new document", async () => {
+        assert.strictEqual(vscode.workspace.textDocuments.length, 2);
+      });
+
+      test("The text of the initial document is not changed", async () => {
+        assert.strictEqual(
+          vscode.window.activeTextEditor.document.getText(),
+          queryText
+        );
+      });
+
+      test("Sends the correct query", async () => {
+        const request = getRequestFromResultDocument();
+
+        assert.strictEqual(request.query, queryText);
+      });
+
+      test("Sends the correct language", async () => {
+        const request = getRequestFromResultDocument();
+
+        assert.strictEqual(request.language, language);
+      });
     });
   });
 });
+
+function getRequestFromResultDocument() {
+  const requestStr = vscode.workspace.textDocuments
+    .find((document) => document !== vscode.window.activeTextEditor.document)
+    .getText();
+  return JSON.parse(requestStr);
+}
