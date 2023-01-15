01825fd3f8fa912001ce002f83a9839fc142382c
VladBrok <vlad.brok99@gmail.com>
2023-01-15 11:05:14 +0300

    snippet.showNextAnswer tests

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -2,36 +2,22 @@ import * as assert from "assert";
 import * as vscode from "vscode";
 import { after, before } from "mocha";
 
-suite("Extension Test Suite", function () {
+suite("Extension Test Suite", () => {
   suite("snippet.findSelectedText", () => {
     suite("openInNewEditor is true", () => {
       const queryText = "query";
       const language = "javascript";
 
-      before(async () => {
-        const document = await vscode.workspace.openTextDocument({
-          language,
-          content: queryText,
-        });
-        await vscode.window.showTextDocument(document);
-        vscode.window.activeTextEditor.selection = new vscode.Selection(
-          0,
-          0,
-          0,
-          queryText.length
-        );
-        const config = vscode.workspace.getConfiguration("snippet");
-        const configTargetIsGlobal = true;
-        await config.update("openInNewEditor", true, configTargetIsGlobal);
+      before(
+        async () =>
+          await openDocumentAndFindSelectedText({
+            language,
+            queryText,
+            openInNewEditor: true,
+          })
+      );
 
-        await vscode.commands.executeCommand("snippet.findSelectedText");
-      });
-
-      after(async () => {
-        await vscode.commands.executeCommand(
-          "workbench.action.closeAllEditors"
-        );
-      });
+      after(closeAllDocuments);
 
       test("Opens a new document", async () => {
         assert.strictEqual(vscode.workspace.textDocuments.length, 2);
@@ -57,6 +43,45 @@ suite("Extension Test Suite", function () {
       });
     });
   });
+
+  suite("snippet.showNextAnswer", () => {
+    suite("openInNewEditor is true", () => {
+      const queryText = "query";
+      const language = "javascript";
+
+      before(async () => {
+        await openDocumentAndFindSelectedText({
+          language,
+          queryText,
+          openInNewEditor: true,
+        });
+      });
+
+      after(closeAllDocuments);
+
+      test("Initial answer number is 0", async () => {
+        const request = getRequestFromResultDocument();
+
+        assert.strictEqual(request.answerNumber, 0);
+      });
+
+      test("Increments answer number", async () => {
+        const maxAnswers = 5;
+        const answerNumbers = Array(maxAnswers).fill(0);
+        const expectedAnswerNumbers = Array(maxAnswers)
+          .fill(0)
+          .map((_, i) => i);
+
+        for (let i = 0; i < maxAnswers; i++) {
+          await vscode.commands.executeCommand("snippet.showNextAnswer");
+          const request = getRequestFromResultDocument();
+          answerNumbers[i] = request.answerNumber;
+        }
+
+        assert.deepEqual(answerNumbers, expectedAnswerNumbers);
+      });
+    });
+  });
 });
 
 function getRequestFromResultDocument() {
@@ -64,4 +89,39 @@ function getRequestFromResultDocument() {
     .find((document) => document !== vscode.window.activeTextEditor.document)
     .getText();
   return JSON.parse(requestStr);
+}
+
+interface Options {
+  language: string;
+  queryText: string;
+  openInNewEditor: boolean;
+}
+
+async function openDocumentAndFindSelectedText(options: Options) {
+  const document = await vscode.workspace.openTextDocument({
+    language: options.language,
+    content: options.queryText,
+  });
+  await vscode.window.showTextDocument(document);
+
+  vscode.window.activeTextEditor.selection = new vscode.Selection(
+    0,
+    0,
+    0,
+    options.queryText.length
+  );
+
+  const config = vscode.workspace.getConfiguration("snippet");
+  const configTargetIsGlobal = true;
+  await config.update(
+    "openInNewEditor",
+    options.openInNewEditor,
+    configTargetIsGlobal
+  );
+
+  await vscode.commands.executeCommand("snippet.findSelectedText");
+}
+
+async function closeAllDocuments() {
+  await vscode.commands.executeCommand("workbench.action.closeAllEditors");
 }
