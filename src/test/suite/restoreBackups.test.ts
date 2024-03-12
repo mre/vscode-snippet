0f8b0c94267bf558095c79addd564934e7476cfd
VladBrok <vlad.brok99@gmail.com>
2024-03-12 15:08:59 +0100

    test that backup is created after rename

--- src/test/suite/restoreBackups.test.ts
+++ src/test/suite/restoreBackups.test.ts
@@ -1,27 +1,14 @@
 import * as assert from "assert";
-import { after, afterEach, before } from "mocha";
+import { afterEach } from "mocha";
 import * as sinon from "sinon";
 import * as vscode from "vscode";
 import { MessageItem } from "vscode";
 import { BackupItem } from "../../backupManager";
 import { cache } from "../../cache";
 import SnippetsStorage, { TreeElement } from "../../snippetsStorage";
-import { closeAllEditors, openDocumentAndFind } from "../testUtils";
 
 suite("snippet.restoreBackups", () => {
   suite("openInNewEditor is true", () => {
-    before(
-      async () =>
-        await openDocumentAndFind({
-          language: "",
-          documentText: "",
-          queryText: "",
-          openInNewEditor: true,
-        })
-    );
-
-    after(closeAllEditors);
-
     afterEach(() => {
       sinon.restore();
     });
@@ -29,6 +16,31 @@ suite("snippet.restoreBackups", () => {
     test("No backups initially", async () => {
       await getBackups((backups: BackupItem[]) => {
         assert.strictEqual(backups.length, 0);
+      });
+    });
+
+    test("Creates a backup after rename", async () => {
+      sinon.stub(vscode.window, "showInputBox").callsFake(() => {
+        return Promise.resolve("new name");
+      });
+
+      const originalElements =
+        cache.state.get<string>("snippet.snippetsStorageKey") || "[]";
+
+      const snippet = (JSON.parse(originalElements) as TreeElement[]).find(
+        (x) => !SnippetsStorage.isFolder(x)
+      );
+
+      await vscode.commands.executeCommand("snippet.renameSnippet", {
+        id: snippet.data.id,
+      });
+
+      await getBackups((backups: BackupItem[]) => {
+        assert.strictEqual(backups.length, 1);
+        assert.strictEqual(
+          JSON.stringify(backups[0].item.elements),
+          originalElements
+        );
       });
     });
 
@@ -49,7 +61,7 @@ suite("snippet.restoreBackups", () => {
       });
 
       await getBackups((backups: BackupItem[]) => {
-        assert.strictEqual(backups.length, 1);
+        assert.strictEqual(backups.length, 2);
         assert.strictEqual(
           JSON.stringify(backups[0].item.elements),
           originalElements
