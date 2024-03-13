b3bf5e083624e1e8bb298a386cdb2e95bd5d1cb1
VladBrok <vlad.brok99@gmail.com>
2024-03-13 09:33:27 +0100

    add "ctx.subscriptions.push" to cleanup the extension state

--- src/extension.ts
+++ src/extension.ts
@@ -18,77 +18,110 @@ export function activate(ctx: vscode.ExtensionContext) {
   new CompletionManager(ctx, snippetsStorage);
   const backupManager = new BackupManager(ctx, snippetsStorage);
 
-  vscode.commands.registerCommand("snippet.find", () =>
-    endpoints.findDefault(snippetsStorage)
+  ctx.subscriptions.push(
+    vscode.commands.registerCommand("snippet.find", () =>
+      endpoints.findDefault(snippetsStorage)
+    )
   );
-  vscode.commands.registerCommand("snippet.findForLanguage", () =>
-    endpoints.findForLanguage(snippetsStorage)
+  ctx.subscriptions.push(
+    vscode.commands.registerCommand("snippet.findForLanguage", () =>
+      endpoints.findForLanguage(snippetsStorage)
+    )
   );
-  vscode.commands.registerCommand("snippet.findInplace", () =>
-    endpoints.findInplace(snippetsStorage)
+  ctx.subscriptions.push(
+    vscode.commands.registerCommand("snippet.findInplace", () =>
+      endpoints.findInplace(snippetsStorage)
+    )
   );
-  vscode.commands.registerCommand("snippet.findInNewEditor", () =>
-    endpoints.findInNewEditor(snippetsStorage)
+  ctx.subscriptions.push(
+    vscode.commands.registerCommand("snippet.findInNewEditor", () =>
+      endpoints.findInNewEditor(snippetsStorage)
+    )
   );
-  vscode.commands.registerCommand(
-    "snippet.findSelectedText",
-    endpoints.findSelectedText
-  );
-  vscode.commands.registerCommand("snippet.showPreviousAnswer", () =>
-    endpoints.showPreviousAnswer(snippetsStorage)
-  );
-  vscode.commands.registerCommand("snippet.showNextAnswer", () =>
-    endpoints.showNextAnswer(snippetsStorage)
-  );
-  vscode.commands.registerCommand(
-    "snippet.toggleComments",
-    endpoints.toggleComments
-  );
-
-  vscode.commands.registerCommand(
-    "snippet.saveSnippet",
-    endpoints.saveSnippet(snippetsTreeProvider)
-  );
-  vscode.commands.registerCommand(
-    "snippet.insertSnippet",
-    endpoints.insertSnippet(snippetsTreeProvider)
-  );
-  vscode.commands.registerCommand(
-    "snippet.deleteSnippet",
-    endpoints.deleteSnippet(snippetsTreeProvider)
-  );
-  vscode.commands.registerCommand(
-    "snippet.renameSnippet",
-    endpoints.renameSnippet(snippetsTreeProvider)
-  );
-  vscode.commands.registerCommand(
-    "snippet.copySnippet",
-    endpoints.copySnippet(snippetsTreeProvider)
-  );
-  vscode.commands.registerCommand(
-    "snippet.findAndCopy",
-    endpoints.findAndCopy(snippetsStorage)
-  );
-  vscode.commands.registerCommand(
-    "snippet.createFolder",
-    endpoints.createFolder(snippetsTreeProvider)
-  );
-  vscode.commands.registerCommand(
-    "snippet.restoreBackups",
-    endpoints.showBackups(backupManager)
-  );
-
-  if (process.env.NODE_ENV === "test") {
+  ctx.subscriptions.push(
     vscode.commands.registerCommand(
-      "snippet.test_moveElement",
-      async (sourceId, targetId) =>
-        await snippetsStorage.moveElement(sourceId, targetId)
+      "snippet.findSelectedText",
+      endpoints.findSelectedText
+    )
+  );
+  ctx.subscriptions.push(
+    vscode.commands.registerCommand("snippet.showPreviousAnswer", () =>
+      endpoints.showPreviousAnswer(snippetsStorage)
+    )
+  );
+  ctx.subscriptions.push(
+    vscode.commands.registerCommand("snippet.showNextAnswer", () =>
+      endpoints.showNextAnswer(snippetsStorage)
+    )
+  );
+  ctx.subscriptions.push(
+    vscode.commands.registerCommand(
+      "snippet.toggleComments",
+      endpoints.toggleComments
+    )
+  );
+
+  ctx.subscriptions.push(
+    vscode.commands.registerCommand(
+      "snippet.saveSnippet",
+      endpoints.saveSnippet(snippetsTreeProvider)
+    )
+  );
+  ctx.subscriptions.push(
+    vscode.commands.registerCommand(
+      "snippet.insertSnippet",
+      endpoints.insertSnippet(snippetsTreeProvider)
+    )
+  );
+  ctx.subscriptions.push(
+    vscode.commands.registerCommand(
+      "snippet.deleteSnippet",
+      endpoints.deleteSnippet(snippetsTreeProvider)
+    )
+  );
+  ctx.subscriptions.push(
+    vscode.commands.registerCommand(
+      "snippet.renameSnippet",
+      endpoints.renameSnippet(snippetsTreeProvider)
+    )
+  );
+  ctx.subscriptions.push(
+    vscode.commands.registerCommand(
+      "snippet.copySnippet",
+      endpoints.copySnippet(snippetsTreeProvider)
+    )
+  );
+  ctx.subscriptions.push(
+    vscode.commands.registerCommand(
+      "snippet.findAndCopy",
+      endpoints.findAndCopy(snippetsStorage)
+    )
+  );
+  ctx.subscriptions.push(
+    vscode.commands.registerCommand(
+      "snippet.createFolder",
+      endpoints.createFolder(snippetsTreeProvider)
+    )
+  );
+  ctx.subscriptions.push(
+    vscode.commands.registerCommand(
+      "snippet.restoreBackups",
+      endpoints.showBackups(backupManager)
+    )
+  );
+  if (process.env.NODE_ENV === "test") {
+    ctx.subscriptions.push(
+      vscode.commands.registerCommand(
+        "snippet.test_moveElement",
+        async (sourceId, targetId) =>
+          await snippetsStorage.moveElement(sourceId, targetId)
+      )
     );
   }
 
   cache.state = ctx.globalState;
   const provider = new SnippetProvider();
-  const disposableProvider =
-    vscode.workspace.registerTextDocumentContentProvider("snippet", provider);
-  ctx.subscriptions.push(disposableProvider);
+  ctx.subscriptions.push(
+    vscode.workspace.registerTextDocumentContentProvider("snippet", provider)
+  );
 }
