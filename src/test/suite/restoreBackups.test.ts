d9afd69aaca76fe908827184903bb9bf716748cd
VladBrok <vlad.brok99@gmail.com>
2024-03-13 08:25:34 +0100

    remove unnecessary suite

--- src/test/suite/restoreBackups.test.ts
+++ src/test/suite/restoreBackups.test.ts
@@ -8,65 +8,63 @@ import { cache } from "../../cache";
 import SnippetsStorage, { TreeElement } from "../../snippetsStorage";
 
 suite("snippet.restoreBackups", () => {
-  suite("openInNewEditor is true", () => {
-    afterEach(() => {
-      sinon.restore();
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  test("No backups initially", async () => {
+    await getBackups((backups: BackupItem[]) => {
+      assert.strictEqual(backups.length, 0);
+    });
+  });
+
+  test("Creates a backup after rename", async () => {
+    sinon.stub(vscode.window, "showInputBox").callsFake(() => {
+      return Promise.resolve("new name");
     });
 
-    test("No backups initially", async () => {
-      await getBackups((backups: BackupItem[]) => {
-        assert.strictEqual(backups.length, 0);
-      });
+    const originalElements =
+      cache.state.get<string>("snippet.snippetsStorageKey") || "[]";
+
+    const snippet = (JSON.parse(originalElements) as TreeElement[]).find(
+      (x) => !SnippetsStorage.isFolder(x)
+    );
+
+    await vscode.commands.executeCommand("snippet.renameSnippet", {
+      id: snippet.data.id,
     });
 
-    test("Creates a backup after rename", async () => {
-      sinon.stub(vscode.window, "showInputBox").callsFake(() => {
-        return Promise.resolve("new name");
-      });
-
-      const originalElements =
-        cache.state.get<string>("snippet.snippetsStorageKey") || "[]";
-
-      const snippet = (JSON.parse(originalElements) as TreeElement[]).find(
-        (x) => !SnippetsStorage.isFolder(x)
+    await getBackups((backups: BackupItem[]) => {
+      assert.strictEqual(backups.length, 1);
+      assert.strictEqual(
+        JSON.stringify(backups[0].item.elements),
+        originalElements
       );
+    });
+  });
 
-      await vscode.commands.executeCommand("snippet.renameSnippet", {
-        id: snippet.data.id,
-      });
-
-      await getBackups((backups: BackupItem[]) => {
-        assert.strictEqual(backups.length, 1);
-        assert.strictEqual(
-          JSON.stringify(backups[0].item.elements),
-          originalElements
-        );
-      });
+  test("Creates a backup after delete", async () => {
+    sinon.stub(vscode.window, "showInformationMessage").callsFake(() => {
+      return Promise.resolve("Yes" as unknown as MessageItem);
     });
 
-    test("Creates a backup after delete", async () => {
-      sinon.stub(vscode.window, "showInformationMessage").callsFake(() => {
-        return Promise.resolve("Yes" as unknown as MessageItem);
-      });
+    const originalElements =
+      cache.state.get<string>("snippet.snippetsStorageKey") || "[]";
 
-      const originalElements =
-        cache.state.get<string>("snippet.snippetsStorageKey") || "[]";
+    const snippet = (JSON.parse(originalElements) as TreeElement[]).find(
+      (x) => !SnippetsStorage.isFolder(x)
+    );
 
-      const snippet = (JSON.parse(originalElements) as TreeElement[]).find(
-        (x) => !SnippetsStorage.isFolder(x)
+    await vscode.commands.executeCommand("snippet.deleteSnippet", {
+      id: snippet.data.id,
+    });
+
+    await getBackups((backups: BackupItem[]) => {
+      assert.strictEqual(backups.length, 2);
+      assert.strictEqual(
+        JSON.stringify(backups[0].item.elements),
+        originalElements
       );
-
-      await vscode.commands.executeCommand("snippet.deleteSnippet", {
-        id: snippet.data.id,
-      });
-
-      await getBackups((backups: BackupItem[]) => {
-        assert.strictEqual(backups.length, 2);
-        assert.strictEqual(
-          JSON.stringify(backups[0].item.elements),
-          originalElements
-        );
-      });
     });
   });
 });
